f31dd1ffb13b7d1813b00fbb2eca610d5036ac35
sunny-dubey <97658562+sunny-dubey@users.noreply.github.com>
2022-10-23 10:35:32 +0530

    Time: 615 ms (32.33%), Space: 159.9 MB (10.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     
-    bool check(int n, int sum,vector<int> &nums,vector<vector<int>> &dp)
+    bool check(int n, int target,vector<int> &nums,vector<vector<int>> &dp)
     {
         if(n==0)
-            return nums[0]==sum;
-        if(sum==0)
+            return nums[0]==target;
+        if(target==0)
             return true;
-        if(dp[n][sum]!=-1)
-            return dp[n][sum];
         
-        bool x = false;
-        if(sum-nums[n]>=0)
-           x = check(n-1,sum-nums[n],nums,dp);
-        bool y = check(n-1,sum,nums,dp);
+        if(dp[n][target]!=-1)
+            return dp[n][target];
         
-        return dp[n][sum] = x or y;
+        bool take = false;
+        if(target-nums[n]>=0)
+            take = check(n-1,target-nums[n],nums,dp);
+        bool not_take = check(n-1,target,nums,dp);
         
+        return dp[n][target] = take or not_take;
     }
     
     bool canPartition(vector<int>& nums) {
         int sum = 0;
-        for(int i=0;i<nums.size();i++)
-            sum+=nums[i];
+        for(auto x: nums)
+            sum+=x;
         if(sum%2)
             return false;
-        int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(sum + 1,-1));
-        return check(n-1,sum/2,nums,dp);
-        
+        vector<vector<int>> dp(nums.size()+1,vector<int>(sum +1,-1));
+        return check(nums.size()-1,sum/2,nums,dp);
     }
 };
