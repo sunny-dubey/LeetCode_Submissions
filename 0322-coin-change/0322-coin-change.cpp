750655ca80c7c2c516305e5acd658a0818a58958
sunny-dubey <97658562+sunny-dubey@users.noreply.github.com>
2022-10-23 16:01:35 +0530

    Time: 337 ms (10.73%), Space: 45.2 MB (25.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -21,10 +21,32 @@ public:
     }
     
     int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
         
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
+        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,0));
         
-        int ans = check(coins.size()-1,coins,amount,dp);
+        for(int i=0;i<=amount;i++)
+        {
+            if(i%coins[0]==0)
+                dp[0][i] = i/coins[0];
+            else
+                dp[0][i] = 1e9;
+        }
+        for(int i=1;i<n;i++)
+        {
+            
+            for(int j=0;j<=amount;j++)
+            {
+                int exclude = dp[i-1][j];
+                int include = INT_MAX;
+                if(j-coins[i]>=0)
+                    include = 1 + dp[i][j-coins[i]];
+                
+                dp[i][j] = min(include,exclude);
+                
+            }
+        }
+        int ans = dp[n-1][amount];
         if(ans>=1e9)
             return -1;
         return ans;
