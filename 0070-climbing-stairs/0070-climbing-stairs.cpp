5ae22b302a654b72fee8105ae0e6dc63de5fd6af
sunny-dubey <97658562+sunny-dubey@users.noreply.github.com>
2022-10-29 23:09:05 +0530

    Time: 3 ms (35.51%), Space: 6.1 MB (40.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     
-    int check(int n, vector<int> &dp)
+    int rec(int n,vector<int> &dp)
     {
-        if(n==0 ||n==1)
+        if(n==0)
             return 1;
-        if(n<0)
-            return 0;
         if(dp[n]!=-1)
             return dp[n];
         
-        return dp[n] = check(n-1,dp) + check(n-2,dp);
+        int ans = 0;
+        for(int i=1;i<=2;i++){
+            if(n-i>=0)
+              ans+=rec(n-i,dp);
+        }
+        return dp[n] = ans;
     }
+    
     int climbStairs(int n) {
-        
         vector<int> dp(n+1,-1);
-        int ans = check(n,dp);
-        return ans;
-        
+        return rec(n,dp);
     }
 };
